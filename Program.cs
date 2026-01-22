3d548394bb005b6b2dcebd628bf74f4a69bfaef2
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-01-21 23:13:58 -0800

    Finished putting it to were it says true or false

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("20");
+            double num1 = double.Parse("20");
+            Console.Write("40");
+            double num2 = double.Parse("40");
+            double sum = 20 + 40;
+            Console.WriteLine($"sum: {sum}");
+            double average = (20 + 40) / 2;
+            Console.WriteLine($"average: {average}");
+            Console.WriteLine(20 + ">" + 40 +":"+ (20 > 40));
+            Console.WriteLine(20 + "==" + 40 + ":" + (20 == 40));
         }
     }
 }
